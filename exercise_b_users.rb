f6b76a7791741c971ea2a1bd7ef3b721ffc13e9b
Cody Abbott <cody.abbott16@gmail.com>
2019-11-20 16:17:35 +0000

    completed tricky push questions

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -55,12 +55,46 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
+
 # 2. Get Erik's hometown
+p users["Erik"][:home_town]
+
 # 3. Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
+
 # 4. Get the species of Avril's pet Monty
+p users["Avril"][:pets][0][:species]
+
 # 5. Get the smallest of Erik's lottery numbers
+erik_numbers = users["Erik"][:lottery_numbers]
+p erik_numbers.sort[0]
+
 # 6. Return an array of Avril's lottery numbers that are even
+avril_numbers = users["Avril"][:lottery_numbers]
+avril_array = []
+for num in avril_numbers
+ if num % 2 == 0
+   avril_array << num
+ end
+end
+p avril_array
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"][:lottery_numbers] << 7
+p users["Erik"][:lottery_numbers]
+
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+p users["Erik"][:home_town]
+
 # 9. Add a pet dog to Erik called "Fluffy"
+erik_new_pet = {
+  :name => "fluffy",
+  :species => "dog"
+}
+
+users["Erik"][:pets] << erik_new_pet
+p users["Erik"][:pets]
+
 # 10. Add another person to the users hash
